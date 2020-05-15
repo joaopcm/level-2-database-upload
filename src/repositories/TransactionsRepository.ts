e6d4e6d6fdec269ac1595d4682507fa539fa09f7
João Melo <jopcmelo@gmail.com>
2020-05-15 00:49:21 -0300

    get challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,39 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accum, item) => {
+        switch (item.type) {
+          case 'income':
+            accum.income += Number(item.value);
+            break;
+
+          case 'outcome':
+            accum.outcome += Number(item.value);
+            break;
+
+          default:
+            break;
+        }
+
+        return accum;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 }
 
